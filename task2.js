998abf376f238cffba300bd8eac8526636a37d02
Ayush Khemani <khemaniayush96@gmail.com>
2025-12-23 01:40:43 +0100

    completd task 2

--- task2.js
+++ task2.js
@@ -1,21 +1,24 @@
-$(window).bind('scroll',function(e){
-    parallaxScroll();
+window.addEventListener("scroll", () => {
+  const s = window.pageYOffset;
+
+  
+  document.querySelector(".layer-1").style.transform = `translateY(${-s*0.15}px)`;
+  document.querySelector(".layer-2").style.transform = `translateY(${-s*0.25}px)`;
+  document.querySelector(".layer-3").style.transform = `translateY(${-s*0.35}px)`;
+  document.querySelector(".layer-4").style.transform = `translateY(${-s*0.45}px)`;
+  document.querySelector(".layer-5").style.transform = `translateY(${-s*0.55}px)`;
+
+ 
+  document.querySelector(".rock-1").style.transform = `translateY(${-s*0.8}px)`;
+  document.querySelector(".rock-2").style.transform = `translateY(${-s*0.6}px)`;
+  document.querySelector(".rock-3").style.transform = `translateY(${-s*0.4}px)`;
+  document.querySelector(".rock-4").style.transform = `translateY(${-s*0.5}px)`;
+  document.querySelector(".rock-5").style.transform = `translateY(${-s*0.7}px)`;
+  document.querySelector(".rock-6").style.transform = `translateY(${-s*0.7}px)`;
+  document.querySelector(".rock-7").style.transform = `translateY(${-s*0.5}px)`;
+  document.querySelector(".rock-8").style.transform = `translateY(${-s*0.2}px)`;
+  document.querySelector(".rock-9").style.transform = `translateY(${-s*0.4}px)`;
+  document.querySelector(".rock-10").style.transform = `translateY(${-s*0.6}px)`;
+  document.querySelector(".rock-11").style.transform = `translateY(${-s*0.3}px)`;
+  document.querySelector(".rock-12").style.transform = `translateY(${-s*0.5}px)`;
 });
-
-function parallaxScroll(){
-   var scrolled = $(window).scrollTop(); 
-   $('.layer-1').css('top',(0-(scrolled*.25))+'px');
-   $('.layer-2').css('top',(0-(scrolled*.5))+'px');
-   $('.layer-3').css('top',(0-(scrolled*.75))+'px');
-   $('.rock-1').css('top',(400-(scrolled*.8))+'px');
-   $('.rock-2').css('top',(200-(scrolled*.6))+'px');
-   $('.rock-3').css('top',(500-(scrolled*.4))+'px'); 
-   $('.rock-4').css('top',(600-(scrolled*.5))+'px'); 
-   $('.rock-5').css('top',(600-(scrolled*.7))+'px'); 
-   $('.rock-6').css('top',(400-(scrolled*.7))+'px'); 
-   $('.rock-7').css('top',(600-(scrolled*.5))+'px'); 
-   $('.rock-8').css('top',(200-(scrolled*.2))+'px');
-   $('.rock-9').css('top',(200-(scrolled*.4))+'px');
-}
-
-
